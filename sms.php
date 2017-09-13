159eb3b65433f1335c1ece69a263e977291dc89b
HAYANI SAID <hayanisaid1995@gmail.com>
2017-09-13 20:30:35 +0100

    Add files via upload

---
+++ sms.php
@@ -0,0 +1,6 @@
+<form method='POST' action='send_sms.php'>
+	Mobile: <input type='number' name='phone'>
+	Text:<textarea name='text'></textarea>
+		<input type='submit' name='submit' Value='Send'>
+
+</form>
